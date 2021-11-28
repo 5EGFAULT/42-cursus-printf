8b997b92973c6f59139a9004087b3c227e1a3561
Ayoub Souinia <asouinia@e2r13p7.1337.ma>
2021-11-28 02:16:35 +0100

    almost done

---
+++ ft_printing_functions.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printing_functions.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asouinia <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/28 02:12:52 by asouinia          #+#    #+#             */
+/*   Updated: 2021/11/28 02:12:55 by asouinia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_putchar(char c)
+{
+	write(1, &c, 1);
+	return (1);
+}
+
+int	ft_putnbr(long n, int counter)
+{
+	if (n < 0)
+	{
+		ft_putchar('-');
+		return (ft_putnbr(n * -1, 0) + 1);
+	}
+	if (n < 10 && n >= 0)
+		return (ft_putchar('0' + n));
+	counter += ft_putnbr(n / 10, counter);
+	counter += ft_putchar('0' + (n % 10));
+	return (counter);
+}
+
+int	ft_putstr(char *str)
+{
+	int	i;
+
+	if (!str)
+		return (ft_putstr("(null)"));
+	i = -1;
+	while (str && str[++i] != '\0')
+		write(1, str + i, 1);
+	return (i);
+}
+
+int	ft_printhex(size_t n, int lower_upper, int counter)
+{
+	char	*set;
+
+	if (lower_upper)
+		set = "0123456789ABCDEF";
+	else
+		set = "0123456789abcdef";
+	if (n < 16 && n >= 0)
+		return (ft_putchar(set[n]));
+	if (n < 0)
+	{
+		ft_putchar('-');
+		return (ft_printhex(n * -1, lower_upper, 1));
+	}
+	counter = ft_printhex(n / 16, lower_upper, counter);
+	ft_putchar(set[n % 16]);
+	counter++;
+	return (counter);
+}
+
+int	ft_address(size_t addr)
+{
+	ft_putchar('0');
+	ft_putchar('x');
+	return (ft_printhex(addr, 0, 0) + 2);
+}
